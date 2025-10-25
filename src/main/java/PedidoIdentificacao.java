731994baa5930e13fa358393968999233d481130
Sophia Franco de Godoy <161141869+sophiagodoy@users.noreply.github.com>
2025-10-25 20:26:09 -0300

    Criação de classe PedidoIdentificacao para identificar usuarios

---
+++ src/main/java/PedidoIdentificacao.java
@@ -0,0 +1,10 @@
+public class PedidoIdentificacao extends Pedido{
+    private String uid;
+    public PedidoIdentificacao(String uid) {
+        this.uid = uid;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+}
